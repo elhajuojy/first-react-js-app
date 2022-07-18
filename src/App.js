2b529b4165df1c5ff57123f8bbef8d56237b8a2b
elhajuojy <souirimehdi311@gmail.com>
2022-07-18 20:42:38 +0100

    step 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 import React from 'react';
 import Header from './view/components/header';
 import Card from './view/components/card';
+import BreakingBadData from './breakingbad.json';
 
 function App() {
+
+  const cards = BreakingBadData.map(function (element) {
+    return <Card title={element.name} img={element.img} content='card content 1 best mehdi' />
+  });
+
   return (
     <div className="container">
       <Header />
-      <Card />
+      {/* <Card title='card title 1' content='card content 1 best mehdi' />
+      <Card title='card title 2' content='card content 2 best mehdi' /> */}
+      <div className='row'>
+        {cards}
+
+      </div>
+
+
+
 
 
     </div>
--- src/view/components/header.js
+++ src/view/components/header.js
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div>
             <header className='navbar navbar-expand-lg bg-dark text-white text rounded'>
-                <h1>this is header</h1>
+                <h1>Breaking bad </h1>
             </header>
         </div>
     )
